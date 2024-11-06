eac589a16634bf775ff17e2f9dcf55d18768c114
WalterZStark <walterzstark@gmail.com>
2024-11-06 03:21:58 -0600

    Removed goal noise

--- scripts/vis_s1.sh
+++ scripts/vis_s1.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 CACHE=$1
-python train.py task=AllegroHandHora headless=False pipeline=gpu \
+python train.py task=FrankaCubePush headless=False pipeline=gpu \
 task.env.numEnvs=1 test=True \
-task.env.object.type=simple_tennis_ball \
 train.algo=PPO \
-task.env.randomization.randomizeMass=False \
-task.env.randomization.randomizeCOM=False \
-task.env.randomization.randomizeFriction=False \
-task.env.randomization.randomizePDGains=False \
-task.env.randomization.randomizeScale=True \
 train.ppo.priv_info=True \
-train.ppo.output_name=AllegroHandHora/"${CACHE}" \
-checkpoint=outputs/AllegroHandHora/"${CACHE}"/stage1_nn/best.pth
+train.ppo.output_name=FrankaCubePush/ \
+checkpoint=outputs/FrankaCubePush/stage1_nn/best.pth
